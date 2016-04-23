90c581b4cfb85075b0f465e94dece741e64c3ec0
Jessie Yang <jessyang90@Jessies-MacBook-Air.local>
2016-04-23 14:00:02 -0700

    WORKING

--- app/app.js
+++ app/app.js
@@ -19,6 +19,25 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+
+    muzik.startServer();
+
+    muzik.registerForConnectionState(function onStateChanged(state) {
+      if (state == muzik.CONNECTION_STATE.CONNECTED) {
+        onConnect();
+      }
+      else if (state == muzik.CONNECTION_STATE.HEADPHONES_NOT_CONNECTED) {
+        // handle
+      }
+      // ...
+    });
+
+    function onConnect() {
+      muzik.getBatteryLevel(function onBatteryLevelReceived(batteryLevel) {
+        alert ("The current battery level is :" + batteryLevel + "%");
+      });
+    }
+
     });
   }
 }
